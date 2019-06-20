92fee9cfeef8640d4cee2fa10c7cdd80be6a3d2a
Oren Ben-Kiki <github-oren@ben-kiki.org>
2019-06-20 13:40:01 +0300

    Adapt to latest mypy 0.710.

--- dynamake/patterns.py
+++ dynamake/patterns.py
@@ -259,14 +259,14 @@ def _load_glob(loader: Loader, node: Node) -> Pattern:
     return re.compile(glob2re(loader.construct_scalar(node)))
 
 
-yaml.add_constructor('!g', _load_glob, Loader=yaml.FullLoader)  # type: ignore
+yaml.add_constructor('!g', _load_glob, Loader=yaml.FullLoader)
 
 
 def _load_regexp(loader: Loader, node: Node) -> Pattern:
     return re.compile(loader.construct_scalar(node))
 
 
-yaml.add_constructor('!r', _load_regexp, Loader=yaml.FullLoader)  # type: ignore
+yaml.add_constructor('!r', _load_regexp, Loader=yaml.FullLoader)
 
 #: An arbitrarily nested list of strings.
 #:
